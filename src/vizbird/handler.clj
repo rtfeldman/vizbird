ac76753b17ca02512a173d6e809e1fb12d3917d6
Richard Feldman <richard.t.feldman@gmail.com>
2014-02-09 17:02:11 -0800

    Add Skeleton and serve it statically.

--- src/vizbird/handler.clj
+++ src/vizbird/handler.clj
@@ -1,8 +1,11 @@
 (ns vizbird.handler
   (:use compojure.core)
+  (:require [ring.util.response :as resp])
   (:require [compojure.route :as route])
   (:require [vizbird.pages :as pages]))
 
 (defroutes app
-  (GET "/" [] (pages/index))
+  (GET "/query" [] (pages/index))
+  (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
+  (route/resources "/")
   (route/not-found "<h1>Page not found</h1>"))
